6029d4c0a037be1eaeaccf446d7bbbc40218c774
kdtTetz <xenosign@naver.com>
2023-01-16 00:53:27 +0900

    Update server.js

--- data-server/server.js
+++ data-server/server.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 const mongoRouter = require('./routes/mongo');
 app.use('/mongo', mongoRouter);
